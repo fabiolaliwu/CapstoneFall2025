3b7657981a00d24d9b5f9a35321ab7df6027620f
Fabiola Li Wu <fabiolaliwu@users.noreply.github.com>
2025-09-23 17:55:28 -0400

    google maps integrated in html based on google maps api documentation

--- website/src/main.jsx
+++ website/src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Homepage from './homePage/homepage'
+import Maps from './app'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Homepage />
+    {/* <Homepage /> */}
+    <Maps />
   </StrictMode>,
 )
