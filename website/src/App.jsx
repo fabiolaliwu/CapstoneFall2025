23e710f0deb18bdb72ae51b2c742981a2ddfa53a
ReinaC26 <chensj0314@gmail.com>
2025-09-28 12:14:10 -0400

    Added slogan and logo to the about page

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -2,10 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from './components/homePage/homepage'
 import About from "./components/aboutPage/about";
 import Help from "./components/helpPage/help";
+import Bar from "./components/homePage/bar";
+import './index.css'
 
 function App() {
     return (
         <Router>
+            <Bar />
             <Routes>
                 <Route path="/" element={<Homepage />} />
                 <Route path="/about" element={<About />} />
--- website/src/components/aboutPage/about.jsx
+++ website/src/components/aboutPage/about.jsx
@@ -1,8 +1,16 @@
 import './about.css';
+import logo from './logo.jpg';
 
 function About(){
     return(
-        <div className ='about'>
+        <div className ='about-page'>
+            <div className='slogan-top'>
+                Stay Safe. Stay Connect.
+            </div>
+            <div className='slogan-bottom'>
+                Stay In the LOOP.
+            </div>
+            <img src={logo} alt="Logo" className="logo-image" />
             This is the about page
         </div>
     )
