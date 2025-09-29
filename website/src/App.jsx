2e70a33794dbd57c84ccca38160423d62ac7cf98
angela <apzhang7@gmail.com>
2025-09-29 00:09:18 -0400

    added routes + full signup / login page and respective css pages

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
 import Homepage from './components/homePage/homepage'
+import Login from "./components/loginPage/login";
+import Signup from "./components/signupPage/signup";
 import About from "./components/aboutPage/about";
 import Help from "./components/helpPage/help";
 import Bar from "./components/homePage/bar";
@@ -11,6 +13,8 @@ function App() {
             <Bar />
             <Routes>
                 <Route path="/" element={<Homepage />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/help" element={<Help />} />
             </Routes>
